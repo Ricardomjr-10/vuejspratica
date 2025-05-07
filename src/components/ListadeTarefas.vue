7e79e4d32767bb98899833de867474e26ad319b7
Ricardo-10 <ricardomjr2@gmail.com>
2025-05-06 22:24:15 -0300

    pratica1

--- src/components/ListadeTarefas.vue
+++ src/components/ListadeTarefas.vue
@@ -4,13 +4,21 @@
         <input type="text" v-model="texto" placeholder="digite um nova tarefa" @keyup.enter="adicionar">
         <button @click="adicionar">Adicionar</button>
         <ul>
-            <li v-for="(lista, index) in listas" :key="index">{{ lista }}</li>
+            <li v-for="(lista, index) in listas" :key="index">
+                <span :class="{concluida: lista.concluida}">{{ lista.texto }}</span>
+                 <button @click="concluida(index)">Concluida
+                    {{ lista.concluida ? 'Desconcluir' : 'Concluir' }}
+                 </button>
+                 <button @click="remover(index)">Remover</button>
+                </li>
         </ul>
     </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+
+
 
     const texto = ref('')
     const listas = ref([])
@@ -21,14 +29,46 @@ import { ref } from 'vue';
             return
         }
 
-        listas.value.push(texto.value)
+        listas.value.push({texto:texto.value.trim(), concluida: false})
         texto.value = ''
+
+    }
+    const concluida = (index) => {
+       listas.value[index].concluida = !listas.value[index].concluida
     }
 
+    const remover = (index) => {
+        listas.value.splice(index, 1)
+    }
+    
 </script>
 
-<style>
+<style scoped>
+.concluida {
+  text-decoration: line-through;
+  color: #888;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
 li {
-    list-style: none;
+  margin-bottom: 8px;
+  padding: 10px;
+  border: 1px solid #eee;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+li span {
+  flex-grow: 1;
+  margin-right: 10px;
+}
+
+li button {
+  margin-left: 5px;
 }
 </style>
