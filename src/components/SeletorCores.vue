4db26d3f6127be2c5de9937593efa74223cebd19
Ricardo-10 <ricardomjr2@gmail.com>
2025-05-09 21:13:34 -0300

    adicionado numero hex

--- src/components/SeletorCores.vue
+++ src/components/SeletorCores.vue
@@ -15,6 +15,7 @@
         </div>
 
         <div class="cor" :style="{backgroundColor: rgbColor}"></div>
+        <p style="margin-top: 10px;">Codigo Hexadecimal: {{ hexColor }}</p>
     </div>
 </template>
     <script setup>
@@ -27,6 +28,14 @@ import { computed, ref } from 'vue';
     const rgbColor = computed(() => {
         return `rgb(${nVerm.value},${nVerde.value},${nAzul.value})`
     }) 
+
+    const hexColor = computed(() => {
+        const toHex = (c) => {
+            const hex = c.toString(16)
+            return hex.length === 1 ? '0' + hex : hex
+        }
+        return `#${toHex(nVerm.value)}${toHex(nVerde.value)}${toHex(nAzul.value)}`
+    })
    </script>
 
 <style scoped>
